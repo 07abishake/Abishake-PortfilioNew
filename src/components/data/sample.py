143d09d496bc86bf74e39b98e9ef81fb0a0ab655
Abishake <143249013+07abishake@users.noreply.github.com>
2024-02-19 20:43:53 +0530

    Update sample.py

--- src/components/data/sample.py
+++ src/components/data/sample.py
@@ -1,10 +1,2 @@
-weight = float(input("hi enter  your   weight: "))
-height = float(input(" hi enter your height: "))
-bmi = weight / (height ** 2)
 
-if bmi < 18.5:
-    print("Underweight")
-elif 18.5 <= bmi < 25:
-    print("Normal")
-elif 25 <= bmi < 30:
-    print("Overweight")
+    
